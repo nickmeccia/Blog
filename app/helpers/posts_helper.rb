86bda6d3125c3fad18dac0ab5285ddb112b6ee76
Nick Meccia <nickmeccia@gmail.com>
2011-09-12 09:49:01 -0500

    cleaned up style.css & index.html.erb

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,10 @@
 module PostsHelper
 
-  def ohs_top title
-    title.gsub("o", "</h1><h3>o</h3><h1>").html_safe
+  def ohs_title title
+    title.gsub("o", "<span class=\"red_o\">o</span>").html_safe
   end
 
-  def ohs_bottom title
-    title.gsub("o", "</h2><h3>o</h3><h2>").html_safe
+  def ohs_date date
+    date.gsub("0", "<span class=\"red_o\">0</span>").html_safe
   end
 end
